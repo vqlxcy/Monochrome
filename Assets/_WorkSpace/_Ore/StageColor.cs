98a4c91f5a35b73f1fca49f74cc3cacde1b2625a
SosukeKato <dokindokinpiano@gmail.com>
2025-12-09 17:38:17 +0900

    [update]白と黒のそれぞれのactiveを切り替える処理を追加

--- Assets/_WorkSpace/_Ore/StageColor.cs
+++ Assets/_WorkSpace/_Ore/StageColor.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class StageColor : MonoBehaviour
 {
-    int _colorControlNumber;
+    [SerializeField]
+    GameObject _whiteParent;
+    [SerializeField]
+    GameObject _blackParent;
+
+    int _colorControlNumber = 0;
 
     bool _white;
     bool _black;
@@ -13,9 +18,13 @@ public class StageColor : MonoBehaviour
         {
             case 0:
                 _white = true;
+                _whiteParent.SetActive(true);
+                _blackParent.SetActive(false);
             break;
             case 1:
                 _black = true;
+                _whiteParent.SetActive(false);
+                _blackParent.SetActive(true);
             break;
         }
         if (Input.GetKeyDown(KeyCode.Return))
