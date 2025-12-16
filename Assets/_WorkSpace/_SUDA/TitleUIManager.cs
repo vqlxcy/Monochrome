ee376217b85ddce146d1ae5a42bb8330b594d376
hha332990 <hha332990@gmail.com>
2025-12-16 12:23:01 +0900

    SE追加可能

--- Assets/_WorkSpace/_SUDA/TitleUIManager.cs
+++ Assets/_WorkSpace/_SUDA/TitleUIManager.cs
@@ -7,16 +7,30 @@ public class TitleUIManager : MonoBehaviour
     public Button playButton;
     public Button explanationButton;
 
+    [Header("SE")]
+    public AudioSource audioSource;
+    public AudioClip buttonSE;
+
     private void Start()
     {
         playButton.onClick.AddListener(() =>
         {
+            PlaySE();
             SceneManager.LoadScene("MonoStage01");
         });
 
         explanationButton.onClick.AddListener(() =>
         {
+            PlaySE();
             SceneManager.LoadScene("MonoExplanation01");
         });
+    }
+
+    void PlaySE()
+    {
+        if (audioSource != null && buttonSE != null)
+        {
+            audioSource.PlayOneShot(buttonSE);
+        }
     }
 }
