2d258bb52b5ce2e07b1e7ba6b4ee99283ade4d7c
hha332990 <hha332990@gmail.com>
2025-12-16 12:28:57 +0900

    ゲームクリアシーンのseも追加

--- Assets/_WorkSpace/_SUDA/GameClearUIManager.cs
+++ Assets/_WorkSpace/_SUDA/GameClearUIManager.cs
@@ -7,16 +7,30 @@ public class GameClearUIManager : MonoBehaviour
     public Button playAgainButton;
     public Button backToTitleButton;
 
+    [Header("SE")]
+    public AudioSource audioSource;
+    public AudioClip buttonSE;
+
     private void Start()
     {
         playAgainButton.onClick.AddListener(() =>
         {
+            PlaySE();
             SceneManager.LoadScene("MonoStage01");
         });
 
         backToTitleButton.onClick.AddListener(() =>
         {
+            PlaySE();
             SceneManager.LoadScene("MonoTitle");
         });
+    }
+
+    void PlaySE()
+    {
+        if (audioSource != null && buttonSE != null)
+        {
+            audioSource.PlayOneShot(buttonSE);
+        }
     }
 }
