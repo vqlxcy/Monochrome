214b1e0a65b352147814400d419b21977a94701a
SosukeKato <dokindokinpiano@gmail.com>
2025-12-22 19:35:10 +0900

    [update]見た目だけ変更する処理を追加

--- Assets/_WorkSpace/_Ore/StageColor.cs
+++ Assets/_WorkSpace/_Ore/StageColor.cs
@@ -3,28 +3,35 @@ using UnityEngine;
 public class StageColor : MonoBehaviour
 {
     [SerializeField,Header("”’‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
-    GameObject _whiteDeleteParent;
-    [SerializeField,Header("•‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
     GameObject _blackDeleteParent;
+    [SerializeField,Header("•‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
+    GameObject _whiteDeleteParent;
+    [SerializeField, Header("—¼•û‚Å‘¶İ‚·‚é‚à‚Ì")]
+    GameObject _monoParent;
 
     public int _colorControlNumber = 0;
+
+    Color _monoParentColor;
 
     bool _white;
     bool _black;
 
     public void ColorChange()
     {
+        _monoParentColor = _monoParent.GetComponentInChildren<SpriteRenderer>().color;
         switch(_colorControlNumber % 2)
         {
             case 0:
                 _white = true;
-                _whiteDeleteParent.SetActive(true);
-                _blackDeleteParent.SetActive(false);
+                _monoParentColor = Color.black;
+                _blackDeleteParent.SetActive(true);
+                _whiteDeleteParent.SetActive(false);
             break;
             case 1:
                 _black = true;
-                _whiteDeleteParent.SetActive(false);
-                _blackDeleteParent.SetActive(true);
+                _monoParentColor = Color.white;
+                _blackDeleteParent.SetActive(false);
+                _whiteDeleteParent.SetActive(true);
             break;
         }
     }
