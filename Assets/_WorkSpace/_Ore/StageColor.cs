359a4300c7e4badebc99307d672638d9dbc1eeb8
SosukeKato <dokindokinpiano@gmail.com>
2026-01-13 22:35:04 +0900

    [update]全ての子オブジェクトの色を変更する処理を追加

--- Assets/_WorkSpace/_Ore/StageColor.cs
+++ Assets/_WorkSpace/_Ore/StageColor.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class StageColor : MonoBehaviour
 {
-    [SerializeField,Header("”’‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
+    [SerializeField, Header("”’‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
     GameObject _blackDeleteParent;
-    [SerializeField,Header("•‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
+    [SerializeField, Header("•‚Ì¢ŠE‚Ì‚É‚¾‚¯Œ©‚¦‚é‚à‚Ì")]
     GameObject _whiteDeleteParent;
     [SerializeField, Header("—¼•û‚Å‘¶İ‚·‚é‚à‚Ì")]
     GameObject _monoParent;
+    [SerializeField, Header("—¼•û‚Ì¢ŠE‚Å‘¶İ‚·‚é‚à‚Ì‚ÌŒ©‚½–Ú(”’‚Ì)")]
+    Sprite _blackBrock;
+    [SerializeField, Header("—¼•û‚Ì¢ŠE‚Å‘¶İ‚·‚é‚à‚Ì‚ÌŒ©‚½–Ú(•‚Ì)")]
+    Sprite _whiteBrock;
 
     public int _colorControlNumber = 0;
 
@@ -23,11 +27,19 @@ public class StageColor : MonoBehaviour
         {
             case 0:
                 _white = true;
+                for (int i = 0; i < _monoParentColor.Length; i++)
+                {
+                    _monoParentColor[i].sprite = _blackBrock;
+                }
                 _blackDeleteParent.SetActive(true);
                 _whiteDeleteParent.SetActive(false);
             break;
             case 1:
                 _black = true;
+                for (int i = 0; i < _monoParentColor.Length; i++)
+                {
+                    _monoParentColor[i].sprite = _whiteBrock;
+                }
                 _blackDeleteParent.SetActive(false);
                 _whiteDeleteParent.SetActive(true);
             break;
