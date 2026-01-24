dae0a27c8fb88415a9559cb570d0b96d11cc501d
SosukeKato <dokindokinpiano@gmail.com>
2026-01-24 18:27:24 +0900

    [update]BulletEnemyの攻撃処理をついか

--- Assets/_WorkSpace/_Ore/BulletEnemy.cs
+++ Assets/_WorkSpace/_Ore/BulletEnemy.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class BulletEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    int _enemyAttackInterval;
+    [SerializeField]
+    GameObject _enemyBullet;
 
-    // Update is called once per frame
+    float _time;
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        if (_time > _enemyAttackInterval)
+        {
+            Instantiate(_enemyBullet);
+            _time = 0;
+        }
     }
 }
