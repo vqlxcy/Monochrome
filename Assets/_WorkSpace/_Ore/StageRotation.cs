f25441767f1879230a98c6471afdc27252836351
SosukeKato <dokindokinpiano@gmail.com>
2026-02-08 22:56:41 +0900

    [fix]ステージを反転させたときPlayerが墜落するのを修正

--- Assets/_WorkSpace/_Ore/StageRotation.cs
+++ Assets/_WorkSpace/_Ore/StageRotation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class StageRotation : MonoBehaviour
 {
@@ -6,11 +7,14 @@ public class StageRotation : MonoBehaviour
     Transform _stage;
     [SerializeField]
     int _rotationAngle;
+    [SerializeField]
+    GameObject _player;
 
     public int _stageRotateCount;
 
     public void StageRotate()
     {
         _stage.eulerAngles = new Vector3(0, 0, _rotationAngle);
+        _player.transform.eulerAngles = new Vector3(0, 0, _rotationAngle);
     }
 }
