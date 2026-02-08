1e3c86fa93d87dadf0f86379f7e20f3042b3d21a
SosukeKato <dokindokinpiano@gmail.com>
2026-02-09 00:27:24 +0900

    [fix]不要な処理を削除

--- Assets/_WorkSpace/_Ore/GameController.cs
+++ Assets/_WorkSpace/_Ore/GameController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -228,11 +227,5 @@ public class GameController : MonoBehaviour
                 _nonCollisionCoinList.Add(obj);
             }
         }
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
     }
 }
