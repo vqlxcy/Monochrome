58f245391d4e59df4ffa132720d6bbb7ecb09f79
SosukeKato <dokindokinpiano@gmail.com>
2026-02-09 00:45:11 +0900

    [update]電動のこぎり追加

--- Assets/_WorkSpace/_Ore/ElectricCutter.cs
+++ Assets/_WorkSpace/_Ore/ElectricCutter.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class ElectricCutter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Transform _tr;
+    StageColor _sc;
+    GameController _gc;
+    BoxCollider2D _c2d;
+    SpriteRenderer _sr;
+
+
+    [SerializeField]
+    int _moveSpeed;
+    [SerializeField]
+    int _alpha;
     void Start()
     {
-        
+        _tr = GetComponent<Transform>();
+        _sc = FindAnyObjectByType<StageColor>();
+        _c2d = GetComponent<BoxCollider2D>();
+        _sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _tr.position += new Vector3(_moveSpeed * Time.deltaTime, 0, 0);
+        if (_tr.position.x >= 10 || _tr.position.x <= -10)
+        {
+            _moveSpeed *= -1;
+        }
+
+        if (_sc._colorControlNumber == 0)
+        {
+            _sr.color = new Vector4(255, 255, 255, 255);
+            _c2d.enabled = true;
+        }
+        else
+        {
+            _sr.color = new Vector4(255, 255, 255, _alpha);
+            _c2d.enabled = false;
+        }
     }
 }
